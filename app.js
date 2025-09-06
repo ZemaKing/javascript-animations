47701c732bc92d6e454a956da677fe1099486e89
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2025-09-06 23:16:34 +0200

    Adding acceleration

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ let circleX = 60;
 let velocity = 1;
 let acceleration = 0.5;
 
+let posY = canvas.height / 2;
+
+function makeRandom(n) {
+	return Math.floor(Math.random() * Math.floor(n));
+}
+
 function canvasAnimation() {
 	circleX++;
-	drawCircle(circleX, canvas.height / 2, 25);
+	// drawCircle(circleX, canvas.height / 2, 25);
+	drawCircle(circleX, posY, 25);
 	canvasInterval = requestAnimationFrame(canvasAnimation);
 }
 
 function drawCircle(x, y, s) {
 	clearCanvas();
+	// context.globalCompositeOperation = "difference";
+	// context.globalCompositeOperation = "screen";
+
 	context.fillStyle = "crimson";
 	context.beginPath();
 	context.arc(x, y, s, 0, 2 * Math.PI);
@@ -22,8 +32,11 @@ function drawCircle(x, y, s) {
 	circleX += velocity;
 
 	if (circleX > canvas.width) {
-		circleX = 0;
-		velocity = 1;
+		// circleX = 0;
+		// velocity = 1;
+		circleX = makeRandom(canvas.width);
+		velocity = makeRandom(3);
+		posY = makeRandom(canvas.height);
 	}
 }
 
