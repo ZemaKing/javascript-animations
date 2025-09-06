8d8b9b92e150f0dd89a9ed17a9fa8242a1399cf4
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2025-09-06 21:47:33 +0200

    Animating within the canvas

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 let canvas = document.getElementById("animated-canvas");
 let context = canvas.getContext("2d");
+let canvasInterval = requestAnimationFrame(canvasAnimation);
+let circleX = 60;
 
-function drawCircle() {
+function canvasAnimation() {
+	circleX++;
+	drawCircle(circleX, canvas.height / 2, 25);
+	canvasInterval = requestAnimationFrame(canvasAnimation);
+}
+
+function drawCircle(x, y, s) {
+	clearCanvas();
 	context.fillStyle = "crimson";
 	context.beginPath();
-	context.arc(60, canvas.height / 2, 25, 0, 2 * Math.PI);
+	context.arc(x, y, s, 0, 2 * Math.PI);
 	context.fill();
+}
+
+function clearCanvas() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 drawCircle();
