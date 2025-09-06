f1db119f9b66f25f3c88e19485777fca310419dc
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2025-09-06 20:37:44 +0200

    Drawing within the canvas context

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let canvas = document.getElementById("canvas");
+let canvas = document.getElementById("animated-canvas");
+let context = canvas.getContext("2d");
+
+function drawCircle() {
+	context.fillStyle = "crimson";
+	context.beginPath();
+	context.arc(60, canvas.height / 2, 25, 0, 2 * Math.PI);
+	context.fill();
+}
+
+drawCircle();
