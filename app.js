e91b5f152d666f75433937cd008260b8fd54f7e5
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2025-09-08 11:04:42 +0200

    Apply easing to the animation

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let rotatingSquare = document.getElementById("web-animation");
 rotatingSquare.animate(
 	[
-		{backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(0deg) translate3D(0, 0, 0)'},
+		{
+			backgroundColor: '#000000',
+			opacity: 1.0,
+			transform: 'rotate(0deg) translate3D(0, 0, 0)',
+			// easing: 'ease-out',
+		},
 		{backgroundColor: '#440000', opacity: 0.3},
-		{backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(360deg) translate3D(0, 0, 0)'},
+		{
+			backgroundColor: '#000000',
+			opacity: 1.0,
+			transform: 'rotate(360deg) translate3D(0, 0, 0)',
+			// easing: 'ease-in',
+		},
 	],
 	{
 		duration: 500,
 		iterations: Infinity,
+		easing: 'ease-in-out',
 	}
 );
