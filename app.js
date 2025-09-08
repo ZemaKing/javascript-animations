b3b5ba7b7f49b565a5ad117edc9d79085729038c
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2025-09-08 11:01:40 +0200

    Using additional keyframe properties

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let rotatingSquare = document.getElementById("web-animation");
 rotatingSquare.animate(
 	[
-		{backgroundColor: '#000000'},
-		{backgroundColor: '#440000'},
-		{backgroundColor: '#000000'},
+		{backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(0deg) translate3D(0, 0, 0)'},
+		{backgroundColor: '#440000', opacity: 0.3},
+		{backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(360deg) translate3D(0, 0, 0)'},
 	],
 	{
-		duration: 1000,
-		iterations: 7,
+		duration: 500,
+		iterations: Infinity,
 	}
 );
